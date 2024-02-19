00587990419af8118a4e6850a24835fb044e43fc
AzimWarsii <azimahmedwarsii3115@gmail.com>
2024-02-19 11:15:26 +0530

    Market

---
+++ app/(tabs)/market.jsx
@@ -0,0 +1,232 @@
+
+import { StatusBar } from 'expo-status-bar';
+import MarketListItem from "../../components/MarketListItem"
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SectionList,
+  SafeAreaView,
+  Image,
+  FlatList,
+  TouchableOpacity
+} from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+
+export default function market(){
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <SectionList
+          contentContainerStyle={{ paddingHorizontal: 10 }}
+          stickySectionHeadersEnabled={false}
+          sections={SECTIONS}
+          renderSectionHeader={({ section }) => (
+            <>
+              <View style={{flexDirection:'row' , marginBottom:8}}>
+               <View  style={styles.iconView}>
+                    <FontAwesome5 name={section.icon} size={20} color="black" />
+                   
+                </View>
+                <Text style={styles.sectionHeader}>{section.title}</Text> 
+                </View>
+              {section.horizontal ? (
+                <FlatList
+                  horizontal
+                  data={section.data}
+                  renderItem={({ item }) => <MarketListItem  item={item} />}
+                  showsHorizontalScrollIndicator={false}
+                />
+                ) : null}
+            </>
+          )}
+          renderItem={({ item, section }) => {
+            if (section.horizontal) {
+              return null;
+            }
+            return <MarketListItem item={item} />;
+          }}
+        />
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const SECTIONS = [
+  {
+    title: 'AquaFarmers',
+    horizontal: true,
+    category:'1',
+    icon:"store",
+    backgroundColor: "#8EC5FC",
+    backgroundImage: "linear-gradient(62deg, #8EC5FC 0%, #d3f0ff 100%)",
+
+    data: [
+      {
+        key: '1',
+        name: 'Name',
+        location:'Location',
+        status:'Verified',
+        uri: 'https://fishfarmtank.com/wp-content/uploads/2019/05/6-home-fish-farming-tank.jpg',
+      },
+      {
+        key: '2',
+        name: 'Name',
+        location:'Location',
+        status:'Not Verified',
+        uri: 'https://ogden_images.s3.amazonaws.com/www.motherearthnews.com/images/2022/05/12163447/recirculating-fish-farm-412x300.jpg',
+      },
+
+      {
+        key: '3',
+        name: 'Name',
+        location:'Location',
+        status:'Verified',
+        uri: 'https://ogden_images.s3.amazonaws.com/www.motherearthnews.com/images/1979/11/22174025/060-home-fish-farming-defun-fotolia.jpg',
+      },
+      {
+        key: '4',
+        name: 'Name',
+        location:'Location',
+        status:'Verified',
+        uri: 'https://s3.amazonaws.com/kajabi-storefronts-production/site/7230/images/vwSRMLTRQz2xAW04kVL4_backyard_fish_farming.jpg',
+      },
+      {
+        key: '5',
+        name: 'Name',
+        location:'Location',
+        status:'Verified',
+        uri: 'https://www.theaquaponicsource.com/wp-content/uploads/2020/10/Fish-Farm-600-Product-Image-scaled.jpg',
+      },
+    ],
+  },
+  {
+    title: 'Fish Food',
+    horizontal: true,
+    category:'2',
+    icon:"fish",
+    data: [
+      {
+        key: '1',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Verified',
+        uri: 'https://content.jdmagicbox.com/comp/rudrapur/j9/9999p5944.5944.160422100923.k6j9/catalogue/nitin-fish-aquarium-khera-rudrapur-aquariums-xj4sgg54zd.jpg',
+      },
+      {
+        key: '2',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Verified',
+        uri: 'https://content.jdmagicbox.com/comp/hyderabad/x7/040pxx40.xx40.130916132145.l4x7/catalogue/gills-and-fins-acquirium-shop-chintal-hyderabad-pet-shops-14avz7t.jpg?clr=',
+      },
+
+      {
+        key: '3',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Not Verified',
+        uri: 'https://media.istockphoto.com/id/687479244/photo/pet-shop-aquarium-with-goldfish.jpg?s=1024x1024&w=is&k=20&c=S44igR9ibHh2XKN3XBr-oWpMEFdOS9tHOVVCrotuQ1Q=',
+      },
+      {
+        key: '4',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Verified',
+        uri: 'https://media.istockphoto.com/id/1344706066/photo/closeup-view-of-fish-food-granules.jpg?s=1024x1024&w=is&k=20&c=YVzamYqD8NMEIGIdHrG2LhmovAM8EFHJG7nXcxA5U70=',
+      },
+      {
+        key: '5',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Verified',
+        uri: 'https://content.jdmagicbox.com/comp/siliguri/e6/9999px353.x353.160927124948.j7e6/catalogue/the-fish-people-hakimpara-siliguri-aquarium-fish-dealers-ea2b9f18ol-250.jpg?clr=',
+      },
+    ],
+  },
+  {
+    title: 'Shop Category',
+    horizontal: true,
+    category:'3',
+    icon:"water",
+    data: [
+      {
+        key: '1',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Verified',
+        uri: '',
+      },
+      {
+        key: '2',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Verified',
+        uri: '',
+      },
+
+      {
+        key: '3',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Verified',
+        uri: '',
+      },
+      {
+        key: '4',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Verified',
+        uri: '',
+      },
+      {
+        key: '5',
+        name: 'Shop Name',
+        location:'Shop Location',
+        status:'Verified',
+        uri: '',
+      },
+    ],
+  },
+];
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  sectionHeader: {
+    fontFamily:'mon-b',
+    //fontWeight: '800',
+    fontSize: 17,
+    color: '#000',
+    marginTop: 27,
+    marginBottom: 5,
+    marginLeft:15
+  },
+  item: {
+    margin: 10,
+  },
+  itemPhoto: {
+    width: 200,
+    height: 200,
+  },
+  itemText: {
+    color: 'rgba(255, 255, 255, 0.5)',
+    marginTop: 5,
+  },
+  iconView: {
+    backgroundColor: "#D4E6F1",
+    borderRadius:17, 
+    width:45, 
+    height: 45, 
+    alignItems:"center", 
+    justifyContent: "center",
+    marginTop: 16,
+    marginBottom: 5,
+    marginLeft:7,
+}
+});
---
+++ components/MarketListItem.jsx
@@ -0,0 +1,126 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SectionList,
+  SafeAreaView,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+const windowWidth = Dimensions.get('window').width;
+
+
+const MarketListItem = ({ item }) => {
+    return (
+      <TouchableOpacity>
+        <View style={styles.item}>
+         
+        <View style={styles.imageContainer}>
+          {item.uri!=""?
+          <Image source={{
+              uri: item.uri,
+            }} style={styles.image} />
+            :
+            <Image source={require('assets/images/store.png')} style={{flex: 1,
+              resizeMode: 'cover',width: '80%',alignSelf:'center'}} />
+            }
+        </View>
+        <View style={styles.textContainer}>
+          <View style={styles.infoContainer}>
+            <Text style={styles.itemName}>{item.name}</Text>
+            <Text style={styles.itemLocation}>{item.location}</Text>
+          </View>
+          <View style={{flexDirection:'row'}} >
+            {item.status=='Verified' &&
+            <>
+            <FontAwesome5 name={"check"}  style={styles.itemStatus}  />
+            <Text style={styles.itemStatus}>{item.status}</Text>
+            </>
+            }
+          </View>
+        </View>
+          
+        </View>
+        </TouchableOpacity>
+      );
+}
+
+export default MarketListItem
+
+const styles = StyleSheet.create({
+    item: {
+      marginHorizontal: 8,
+       backgroundColor: "#fff",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.34,
+        shadowRadius: 3,
+        elevation: 10,
+        margin: 5,
+        height: 230,
+        borderRadius: 15,
+        fontFamily: "mon-sb",
+    },
+    itemPhoto: {
+      width: 200,
+      height: 200,
+    },
+    itemName: {
+      fontFamily:'mon-sb',
+      color: '#000',
+      fontSize:16,  
+      margin: 5,
+    },
+    itemLocation: {
+      fontFamily:'mon-sb',
+      color: '#0007',
+      fontSize:13,  
+      marginLeft: 5,
+    },
+    itemStatus: {
+      //height:14,
+      //position:'absolute',
+      //bottom:14,
+      fontFamily:'mon-sb',
+      color: '#1e90ff',
+      fontSize:13,  
+      marginLeft: 5,
+    },
+    itemStatusFalse: {
+      fontFamily:'mon-sb',
+      color: '#FF474C',
+      fontSize:13,  
+      marginLeft: 5,
+    },
+    imageContainer: {
+      backgroundColor:'#0005',
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      height: 110,
+      width: windowWidth - 230,
+
+    },
+    infoContainer:{
+      height:92,
+      width: windowWidth - 230,
+    },
+    textContainer:{
+      height:120,
+      width: windowWidth - 230,
+    },
+    image: {
+       borderTopLeftRadius: 15,
+       borderTopRightRadius: 15,
+      flex: 1,
+      resizeMode: 'cover',
+      width: '100%',
+    }
+  });
